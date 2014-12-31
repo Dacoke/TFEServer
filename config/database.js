6b42765ed22e718bc3e884273ac3cc58e024bfc0
Kev <kev.dacosta.nims@gmail.com>
2014-12-31 17:37:08 +0100

    first commit including all the files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://dacoke:mtfemclc@proximus.modulusmongo.net:27017/y2mywaMa' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
