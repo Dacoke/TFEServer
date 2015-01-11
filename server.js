46c5837c91bd598f396a6c333c25c31dfe6c3b7c
Kev <kev.dacosta.nims@gmail.com>
2015-01-11 20:20:16 +0100

    changed ports from 8080 to 80

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app      = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 80;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var morgan       = require('morgan');
