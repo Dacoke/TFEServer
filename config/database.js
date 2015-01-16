43db66dc238e3c164a30a57a86be49031227ef73
Kev <kev.dacosta.nims@gmail.com>
2015-01-16 03:42:44 +0100

    added meta tag with viewport in head of all pages to add responsiveness

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
 module.exports = {
 
     'url' : 'mongodb://dacoke:mtfemclc@proximus.modulusmongo.net:27017/y2mywaMa' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+    																			//Modulus.io
 };
